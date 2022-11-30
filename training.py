1e3c3e8c9ae6832a0c1e0433dbadc84f2c26fd4f
AlexSteiner30 <65250433+AlexSteiner30@users.noreply.github.com>
2022-11-30 11:13:01 +0100

    Better

--- training.py
+++ training.py
@@ -1,23 +1,13 @@
 from detecto import core, utils
 from torchvision import transforms
 import matplotlib.pyplot as plt
+from detecto.core import Dataset, Model, DataLoader
 
-from detecto.core import Dataset
+dataset = Dataset('your_images_and_labels/') #folder (traning)
 
-dataset = Dataset('your_images_and_labels/')
-dataset = Dataset('your_labels/', 'your_images/')
-
-from detecto.utils import xml_to_csv
-
-xml_to_csv('your_labels/', 'labels.csv')
-dataset = Dataset('labels.csv', 'your_images/')
-
-from detecto.core import DataLoader, Model
-
-# Specify all unique labels you're trying to predict
-your_labels = ['label1', 'label2', '...']
+your_labels = ['label1', 'label2', '...'] # all the labels
 model = Model(your_labels)
 
 model.fit(dataset, verbose=True)
 
-model.save("model.pth")
+model.save("model.pth") 
