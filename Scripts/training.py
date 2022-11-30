4bb82bfa1a06308880d33e2f52c29ad4f9a71a54
AlexSteiner30 <65250433+AlexSteiner30@users.noreply.github.com>
2022-11-30 23:36:24 +0100

    Model Update

--- Scripts/training.py
+++ Scripts/training.py
@@ -1,6 +1,6 @@
 from detecto.core import Dataset, Model, DataLoader
 
-dataset = Dataset('train/') #folder (traning)
+dataset = Dataset('../TrainingModel/') #folder (traning)
 
 your_labels = ['plasticBottle', 'plasticCap', 'plasticBag', 'paperBag'] # all the labels
 model = Model(your_labels)
