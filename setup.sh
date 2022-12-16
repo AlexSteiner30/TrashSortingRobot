aff66263e9035a1b0fa8d8ca1fc884159475fe4d
AlexSteiner30 <65250433+AlexSteiner30@users.noreply.github.com>
2022-12-16 21:25:28 +0100

    Setup.sh Fix

--- setup.sh
+++ setup.sh
@@ -1,19 +1,6 @@
 # Raspberry Pi Setup
 
-sudo apt-get install gfortran
-sudo apt-get install libhdf5-dev libc-ares-dev libeigen3-dev
-sudo apt-get install libatlas-base-dev libopenblas-dev libblas-dev
-sudo apt-get install openmpi-bin libopenmpi-dev
-sudo apt-get install liblapack-dev cython
-sudo pip3 install keras_applications==1.0.8 --no-deps
-sudo pip3 install keras_preprocessing==1.1.0 --no-deps
-sudo pip3 install -U --user six wheel mock
-sudo -H pip3 install pybind11
-sudo -H pip3 install h5py==2.10.0
-
-sudo -H pip3 install --upgrade setuptools
-
-wget https://github.com/Qengineering/Tensorflow-Raspberry-Pi/raw/master/tensorflow-2.1.0-cp37-cp37m-linux_armv7l.whl
-
-sudo -H pip3 install tensorflow-2.1.0-cp37-cp37m-linux_armv7l.whl
-sudo reboot
+sudo apt upgrade
+sudo apt update
+sudo apt install libatlas-base-dev
+pip3 install --upgrade https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.9.0-py3-none-any.whl
