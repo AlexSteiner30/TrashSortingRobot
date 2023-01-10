fde9d65bed54b26d6d4c295e378353eb01cb81db
AlexSteiner30 <65250433+AlexSteiner30@users.noreply.github.com>
2023-01-10 12:17:49 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 # Raspberry Pi Setup
-
-sudo apt upgrade
 sudo apt update
+sudo apt upgrade
+
+# Install pip3
+sudo apt install python3-pip
+
 sudo apt install libatlas-base-dev
+
 pip3 install --upgrade https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.9.0-py3-none-any.whl
+
+# Check if tenserflow was installed
+pip3 show tensorflow
