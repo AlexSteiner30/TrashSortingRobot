1f1cac056155eb3373c89d0326e5e7333c70a1d6
Epic-legend128 <fotis.vaitsopoulos2008@gmail.com>
2023-05-12 12:38:09 +0200

    Better names

--- Scripts/robot.py
+++ Scripts/robot.py
@@ -9,12 +9,12 @@ def move_until(dist):
     while (checkdist() > dist):
         move_motors()
 
-def turnRight(deg):
+def turn_right(deg):
     while (current_degrees < deg):
         move_left_forward()
         move_right_backward()
 
-def turnLeft(deg):
+def turn_left(deg):
     while (current_degrees < deg):
         move_left_backward()
         move_right_forward()
@@ -31,15 +31,15 @@ def move(toWhere):
     
     if ((start_pos == (0, -1) and new_pos == (1, 0)) or (start_pos == (1, 0) and new_pos == (0, 1)) or (start_pos == (0, 1) and new_pos == (-1, 0)) or (start_pos == (-1, 0) and new_pos == (0, -1))):
         move_until(MIDDLE)
-        turnRight(90)
+        turn_right(90)
     elif ((start_pos == (0, -1) and new_pos == (-1, 0)) or (start_pos == (-1, 0) and new_pos == (0, 1)) or (start_pos == (0, 1) and new_pos == (1, 0)) or (start_pos == (1, 0) and new_pos == (0, -1))):
         move_until(MIDDLE)
-        turnLeft(90)
+        turn_left(90)
     elif (start_pos == (0, 0)):
         if (new_pos == (-1, 0)):
-            turnLeft(90)
+            turn_left(90)
         elif (new_pos == (1, 0)):
-            turnRight(90)
+            turn_right(90)
         elif (new_pos == (0, -1)):
-            turnRight(180)
+            turn_right(180)
     move_until(STOP)
